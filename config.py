02d69a16e387b87fcdd7d405c2592475559b8145
MERDHANI <80225366+ramadhani892@users.noreply.github.com>
2022-10-04 05:21:55 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,54 @@
+# Thanks For: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/ramadhani892/RamPyro-Bot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/ramsupportt
+
+from base64 import b64decode as kodenya
+from distutils.util import strtobool
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "👑")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/40a0ecc9b7f5083800118.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
+API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", ""))
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+if not BLACKLIST_CHAT:
+    BLACKLIST_CHAT = [-1001473548283]
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BOT_VER = "1.1.5@main"
+BRANCH = "main"
+CHANNEL = getenv("CHANNEL", "Karc0de")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+DB_URL = getenv("DATABASE_URL", "")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    kodenya("Z2hwX3JMcFhISkYwQTRRZUV0MFhDamM4cWhKeDI2NWVsejNkOGZwMA==").decode(
+        "utf-8"
+    ),
+)
+GROUP = getenv("GROUP", "obrolansuar")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
+REPO_URL = getenv(
+    "REPO_URL",
+    kodenya("aHR0cHM6Ly9naXRodWIuY29tL1RoZVBvd2VyT2ZLYW5nL3Vib3Q=").decode("utf-8"),
+)
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
